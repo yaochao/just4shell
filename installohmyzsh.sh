c8e7441ddb98b2fef07cd6c63c0e6e15f38485d7
yaochao <yaochao365@qq.com>
2016-08-25 14:45:24 +0800

    add sh

---
+++ installohmyzsh.sh
@@ -0,0 +1,23 @@
+main() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+
+  printf "${GREEN}"
+  echo '         __                                     __   '
+  echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+  echo ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+  echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+  echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+  echo '                        /____/                       ....is now installed!'
+  echo ''
+  echo ''
+  echo 'Please look over the ~/.zshrc file to select plugins, themes, and options.'
+  echo ''
+  echo 'p.s. Follow us at https://twitter.com/ohmyzsh.'
+  echo ''
+  echo 'p.p.s. Get stickers and t-shirts at http://shop.planetargon.com.'
+  echo ''
+  printf "${NORMAL}"
+}
+
+main
---
+++ joo.sh
@@ -0,0 +1,19 @@
+  main() {
+    echo '         __                                     __   '
+    echo '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
+    echo ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
+    echo '/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '
+    echo '\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '
+    echo '                        /____/                       .
+    echo ''
+    echo ''
+    echo 'Please look over the ~/.zshrc file to select plugins, themes, and options.'
+    echo ''
+    echo 'p.s. Follow us at https://twitter.com/ohmyzsh.'
+    echo ''
+    echo 'p.p.s. Get stickers and t-shirts at http://shop.planetargon.com.'
+    echo ''
+    say 'deng qiang xi shi yi ga shabbbbbbi'
+    echo '👌'
+  }
+  main
